22a0f341af9e6d025b869bbbd4c67e48ff15f8c0
nahuel61920 <estebancarrizo619@gmail.com>
2024-08-03 00:46:18 -0300

    feat: :sparkles: Endpoint categorias

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,16 +30,16 @@ class AuthController extends Controller
             $user = User::where('email', $request->email)->first();
 
             if (! $user || ! Hash::check($request->password, $user->password)) {
-                return ApiResponse::create('Invalid credentials', 401);
+                return ApiResponse::create('Credenciales no válidas', 401);
             }
 
             // Generar el token usando el servicio TokenService
             $token = $this->tokenService->generateToken($user);
 
-            return ApiResponse::create('Login successful', 200, $token);
+            return ApiResponse::create('Inicio de sesión exitoso', 200, $token);
 
         } catch (Exception $e) {
-            return ApiResponse::create('Error logging in', 500);
+            return ApiResponse::create('Error al iniciar sesión', 500);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductCategoryController;
 
 Route::get('/', function () {
     return 'welcome';
@@ -12,3 +13,7 @@ Route::get('/', function () {
 Route::post('/users', [UserController::class, 'store']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/categories', [ProductCategoryController::class, 'index']);
+Route::post('/categories', [ProductCategoryController::class, 'store']);
+Route::put('/categories/{id}', [ProductCategoryController::class, 'update']);
