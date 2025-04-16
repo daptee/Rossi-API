8f6302c0917860b5ee9d41f99eaa274ad8ca76b2
nahuel61920 <estebancarrizo619@gmail.com>
2025-04-16 17:43:47 -0300

    refactor: :recycle: ImageHelper

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -84,38 +84,3 @@ class ImageHelper
         return $path . $fileName;
     }
 }
-
-<?php
-
-namespace App\Helpers;
-
-use Illuminate\Support\Str;
-use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Gd\Driver; // o Intervention\Image\Drivers\Imagick\Driver
-
-class ImageHelper
-{
-    public static function saveReducedImage($imageFile, $path): string
-    {
-        if (!$imageFile || !$imageFile->isValid()) {
-            throw new \Exception("Archivo de imagen no válido");
-        }    
-        // Crear instancia de ImageManager con driver GD
-        $imageManager = new ImageManager(new Driver());
-
-        $fileName = time() . '_' . Str::random(10) . '_thumbnail' . '.' . $imageFile->getClientOriginalExtension();
-        $fullPath = public_path($path . $fileName);
-
-        if (!file_exists(public_path($path))) {
-            mkdir(public_path($path), 0777, true);
-        }
-
-        // Crear y guardar imagen reducida
-        $imageManager->read($imageFile)
-            ->scaleDown(width: 1280) // ajustá el tamaño
-            ->toJpeg(quality: 75)
-            ->save($fullPath);
-
-        return $path . $fileName;
-    }
-}
